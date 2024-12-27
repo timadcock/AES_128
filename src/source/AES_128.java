140922a5971ccdadc0510eddaae89a2fb248dda4
timadcock <timthetiger98@yahoo.com>
2024-12-26 21:12:06 -0600

    Decryption now works as intended.
    AES_128: Class to perform AES128 decryption and encryption
    
    decrypt: General class function for decryption.
    round_decrypt: Single round decryption.
    inverse_mix_columns: Inverted mix columns.
    inverse_subbyte: Convert matrix to inverted subbyte correlation.
    inverse_shift_rows: Inverted function to shift rows.
    
    Removed potentially problematic helper functions.
    
    TODO:
    Add encryption tests
    Add decryption tests

--- src/source/AES_128.java
+++ src/source/AES_128.java
@@ -45,18 +45,79 @@ public class AES_128 {
     }
 
 
-//    public String decrypt(String cypher_text) {
-//        int[][] decrypted_matrix = Utils.text_to_matrix(cypher_text);
-//        decrypted_matrix = this.addRoundKey(decrypted_matrix, 9);
-//        decrypted_matrix = this.inverse_shift_rows(decrypted_matrix);
-//        decrypted_matrix = this.inverse_subbyte(decrypted_matrix);
-//
-//        for (int i = 9; i > 0; i--) {
-//            decrypted_matrix = this.round_decrypt(decrypted_matrix,i);
-//        }
-//
-//    }
+    /**
+     * Decrypts a cipher texts.
+     *
+     * @param cypher_text Text to decrypt.
+     * @return Decrypted text.
+     */
+    public String decrypt(String cypher_text) {
+        int[][] decrypted_matrix = Utils.text_to_matrix(cypher_text);
+        decrypted_matrix = this.addRoundKey(decrypted_matrix,
+                                            this.round_keys[9]
+        );
 
+        decrypted_matrix = this.inverse_shift_rows(decrypted_matrix);
+        decrypted_matrix = this.inverse_subbyte(decrypted_matrix);
+
+        for (int i = 9; i > 0; i--) {
+            decrypted_matrix = this.round_decrypt(decrypted_matrix,
+                                                  this.round_keys[i]
+            );
+        }
+
+        decrypted_matrix = this.addRoundKey(decrypted_matrix,
+                                            this.round_keys[0]
+        );
+
+        return Utils.matrix_to_string(decrypted_matrix);
+    }
+
+    /**
+     * Performs the steps needed to decrypt a single round.
+     *
+     * @param decryptedMatrix Current matrix being worked on.
+     * @param roundKey Key for this round.
+     * @return Decrypted round matrix.
+     */
+    private int[][] round_decrypt(int[][] decryptedMatrix, int[][] roundKey) {
+
+        decryptedMatrix = this.addRoundKey(decryptedMatrix, roundKey);
+        decryptedMatrix = this.inverse_mix_columns(decryptedMatrix);
+        decryptedMatrix = this.inverse_shift_rows(decryptedMatrix);
+
+        return this.inverse_subbyte(decryptedMatrix);
+    }
+
+    /**
+     * Inverse of mix_columns.
+     *
+     * @param decryptedMatrix Matrix being mixed.
+     * @return Mixed matrix.
+     */
+    private int[][] inverse_mix_columns(int[][] decryptedMatrix) {
+        int[][] output = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            int a = this.xtime(
+                    this.xtime(decryptedMatrix[i][0] ^ decryptedMatrix[i][2]));
+            int b = this.xtime(
+                    this.xtime(decryptedMatrix[i][1] ^ decryptedMatrix[i][3]));
+            output[i][0] = decryptedMatrix[i][0] ^ a;
+            output[i][1] = decryptedMatrix[i][1] ^ b;
+            output[i][2] = decryptedMatrix[i][2] ^ a;
+            output[i][3] = decryptedMatrix[i][3] ^ b;
+        }
+        return this.mix_columns(output);
+    }
+
+
+    /**
+     * Performs the steps needed to encrypt a single round.
+     *
+     * @param encryptedMatrix Matrix to be encrypted.
+     * @param key Key for this round.
+     * @return Encrypted matrix.
+     */
     private int[][] round_encrypt(int[][] encryptedMatrix, int[][] key) {
 
         encryptedMatrix = this.subbyte(encryptedMatrix);
@@ -67,6 +128,12 @@ public class AES_128 {
 
     }
 
+    /**
+     * The procedure to mix the columns of the plaintext.
+     *
+     * @param m Matrix to be mixed.
+     * @return Mixed Matrix.
+     */
     private int[][] mix_columns(int[][] m) {
         for (int i = 0; i < 4; i++) {
             m[i] = this.mix_single_column(m[i]);
@@ -74,6 +141,12 @@ public class AES_128 {
         return m;
     }
 
+    /**
+     * Helper function to mix a single column
+     *
+     * @param c Column to be mixed.
+     * @return Mixed column.
+     */
     private int[] mix_single_column(int[] c) {
         int[] new_column = new int[4];
         int   a          = c[0] ^ c[1] ^ c[2] ^ c[3];
@@ -190,12 +263,18 @@ public class AES_128 {
         return output;
     }
 
+    /**
+     * Inverted function for subbyte.
+     *
+     * @param d Matrix to be converted.
+     * @return Converted matrix.
+     */
     private int[][] inverse_subbyte(int[][] d) {
         int[][] output = new int[4][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                output[j][i] = Utils.INVERSE_SUBBYTE[d[i][j]];
+                output[i][j] = Utils.INVERSE_SUBBYTE[d[i][j]];
             }
         }
         return output;
@@ -234,43 +313,36 @@ public class AES_128 {
         return output;
     }
 
+    /**
+     * Inverted shift_rows
+     *
+     * @param d Matrix to be shifted.
+     * @return Shifted matrix.
+     */
     private int[][] inverse_shift_rows(int[][] d) {
         int[][] output = new int[4][4];
 
-        for (int i = 1; i < 4; i++) {
-            output[0][i] = d[4 - i][i];
-        }
-
+        output[0][1] = d[3][1];
         output[1][1] = d[0][1];
-        output[1][2] = d[3][1];
-        output[1][3] = d[2][1];
-
         output[2][1] = d[1][1];
-        output[2][2] = d[0][1];
-        output[2][3] = d[3][1];
-
         output[3][1] = d[2][1];
-        output[3][2] = d[1][1];
-        output[3][3] = d[0][1];
+
+        output[0][2] = d[2][2];
+        output[1][2] = d[3][2];
+        output[2][2] = d[0][2];
+        output[3][2] = d[1][2];
+
+        output[0][3] = d[1][3];
+        output[1][3] = d[2][3];
+        output[2][3] = d[3][3];
+        output[3][3] = d[0][3];
+
+        output[0][0] = d[0][0];
+        output[1][0] = d[1][0];
+        output[2][0] = d[2][0];
+        output[3][0] = d[3][0];
 
 
         return output;
-    }
-
-
-    public String toString() {
-        return this.matrix_to_string(this.private_key);
-    }
-
-    private String matrix_to_string(int[][] input) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input[i].length; j++) {
-                sb.append(input[i][j] + " ");
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 }
